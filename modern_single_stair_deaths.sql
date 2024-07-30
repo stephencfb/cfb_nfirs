506e707fe3f34d2fe376f1b41e29bbd893f2269b
stephencfb <stephen@centerforbuilding.org>
2024-07-30 17:05:52 -0400

    Update modern_single_stair_deaths.sql

--- modern_single_stair_deaths.sql
+++ modern_single_stair_deaths.sql
@@ -3,8 +3,9 @@ WITH single_stair_plutos AS (
         plutos.*
     FROM pluto_records AS plutos
     WHERE plutos.numfloors BETWEEN 4 AND 6 -- Taller than IBC allows, within NYC BC single-stair height limit
-    AND plutos.yearbuilt >= 2000 -- Post-sprinkler requirement
-    AND (plutos.unitsres >= 3 -- At least three units (definitely has sprinklers)
+    -- AND plutos.yearbuilt >= 2000 -- Post-sprinkler requirement
+    AND -- Single-stair buildings...
+    (plutos.unitsres >= 3 -- At least three units (definitely has sprinklers)
     AND plutos.yearbuilt >= 1968 -- Built since single-stair allowed in 1968 BC
     AND ((plutos.lotarea * 0.6 <= 2000 AND plutos.lottype != '3') OR plutos.lotarea <= 2000) -- R6 districts (which allow the heights that single-stair buildings are built to) allow 60% lot coverage if the building is not on a corner (lottype = 3), so we back into the max allowed floor size from there; otherwise, we assume it might be on a corner and do not allow lot area to exceed 2,000 sq. ft.
     AND plutos.bldgarea / plutos.numfloors <= 2000) -- Average floor size is at most 2,000 sq. ft.
@@ -15,16 +16,19 @@ WITH single_stair_plutos AS (
         basic_fires.fire_service_deaths,
         basic_fires.nfirs_basic_incident_id,
         basic_fires.nfirs_fire_incident_id,
+        basic_fires.incident_type,
         address_plutos.pluto_record_id
     FROM nfirs_basic_fire_incidents AS basic_fires
     JOIN nfirs_incident_addresses AS addresses
     ON basic_fires.nfirs_basic_incident_id = addresses.nfirs_basic_incident_id
     JOIN nfirs_incident_address_pluto_records AS address_plutos
     ON addresses.id = address_plutos.nfirs_incident_address_id
-    WHERE basic_fires.other_deaths > 0 OR basic_fires.fire_service_deaths > 0
+    WHERE (basic_fires.other_deaths > 0 OR basic_fires.fire_service_deaths > 0)
+    AND basic_fires.incident_type = '111'
 )
 
 SELECT
+    nfirs_deaths.incident_type,
     nfirs_deaths.nfirs_basic_incident_id,
     nfirs_deaths.nfirs_fire_incident_id,
     nfirs_deaths.alarm_at,
