0e94ce417d2a4a514977b49309f5d82d469c2b45
stephencfb <stephen@centerforbuilding.org>
2024-07-30 22:07:01 -0400

    Update sprinklered_single_stair_building_unit_counts.sql

--- sprinklered_single_stair_building_unit_counts.sql
+++ sprinklered_single_stair_building_unit_counts.sql
@@ -7,3 +7,8 @@ AND -- NOT single-stair buildings...
 AND plutos.yearbuilt >= 2000 -- Built since single-stair allowed in 1968 BC
 AND ((plutos.lotarea * 0.6 <= 2000 AND plutos.lottype != '3') OR plutos.lotarea <= 2000) -- R6 districts (which allow the heights that single-stair buildings are built to) allow 60% lot coverage if the building is not on a corner (lottype = 3), so we back into the max allowed floor size from there; otherwise, we assume it might be on a corner and do not allow lot area to exceed 2,000 sq. ft.
 AND plutos.bldgarea / plutos.numfloors <= 2000) -- Average floor size is at most 2,000 sq. ft.
+
+
+-- RESULTS
+-- total_unit_years  total_units   total_buildings
+-- 305,905.0	      25,899.0	    4,088
