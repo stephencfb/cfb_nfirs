390cd2e60c99d85f6bff45ad9ce5cd5cc1fce9c7
stephencfb <stephen@centerforbuilding.org>
2024-10-04 13:42:57 -0400

    Update residential_buildings.sql

--- residential_buildings.sql
+++ residential_buildings.sql
@@ -1,3 +1,5 @@
+-- This one's pretty simple – get the total number of residential units in NYC, the total building area thereof, and the total number of buildings
+
 SELECT
     SUM(plutos.unitsres) AS total_unitsres, SUM(plutos.bldgarea) AS total_bldgarea, COUNT(plutos.*) AS total_buildings
 FROM pluto_records AS plutos
