1979fc26acb8b5c84b24d7bfdb0c2640d8011545
stephencfb <stephen@centerforbuilding.org>
2025-03-12 15:52:45 -0400

    Update single_stair_buildings.sql

--- single_stair_buildings.sql
+++ single_stair_buildings.sql
@@ -9,4 +9,4 @@ AND -- Single-stair buildings...
 (plutos.unitsres >= 3 -- At least three units (definitely has sprinklers, per NYC BC)
 AND plutos.yearbuilt >= 1968 -- Built since single-stair allowed in 1968 BC
 AND ((plutos.lotarea * 0.6 <= 2000 AND plutos.lottype != '3') OR plutos.lotarea <= 2000) -- R6 zoning districts (which allow the heights that single-stair buildings are built to) allow 60% lot coverage if the building is not on a corner (lottype = 3), so we back into the max allowed floor size from there; otherwise, we assume it might be on a corner and do not allow lot area to exceed 2,000 sq. ft.
-AND plutos.bldgarea / plutos.numfloors <= 2000) -- Average floor size is at most 2,000 sq. ft.
+AND plutos.bldgarea / plutos.numfloors <= 2000) -- Average floor size is at most 2,000 sq. ft. (the code section states that no upper floor can be more than 2,000 sq. ft., but this is as close as we can get, and in any case it's mostly redundant with the above line, and required or even allowed stepbacks in these districts are minimal)
